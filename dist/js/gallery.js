30e7bb1c3381f10c62264a63677888307537e577
Schöffer Rudolf <spureszka@gmail.com>
2020-02-11 16:00:15 +0100

    sm mod

--- dist/js/gallery.js
+++ dist/js/gallery.js
@@ -1,9 +1,9 @@
-var valami = document.getElementsByClassName("valamikep");
+let img = document.getElementsByClassName("imgChange");
 function change(value) {
     if (value === "1") {
-        for (var i = 0; i < valami.length; i++) {
-        valami[i].classList.toggle("col-lg-12");
-        valami[i].classList.toggle("col-md-12");
+        for (var i = 0; i < img.length; i++) {
+        img[i].classList.toggle("col-lg-12");
+        img[i].classList.toggle("col-md-12");
         }
     }  else {
         console.log("Error")
